ff3aa9a60448af4f546e8606274ae89753aeef81
Spoc Web <10584012+SpocWeb@users.noreply.github.com>
2026-04-06 21:45:46 +0200

    auto-commit

--- GitPull.ps1
+++ GitPull.ps1
@@ -6,22 +6,21 @@ if (-not $parent_directory) {
 	$parent_directory = Split-Path -Path $MyInvocation.MyCommand.Path -Parent # or $PWD
 }
 
+. .\Resolve-Rebase.ps1 
+
 Set-Location -Path $parent_directory
-git pull
+Resolve-Rebase $parent_directory   
+Invoke-SafePull $parent_directory
+#git pull --rebase --strategy-option=theirs
 
 Get-ChildItem -Path $parent_directory -Recurse -Directory | ForEach-Object {
     $directory = $_.FullName
     if (Test-Path "$directory/.git") {
-        Write-Host "Updating repository in $directory"
+        Write-Host "Pulling repository in $directory"
         Set-Location -Path $directory
-        git pull
-        #$stashList = git stash list
-        $latestStash = git stash list | Select-Object -First 1
-        if ($latestStash -match 'autostash') {
-        #if ($stashList -match 'autostash') {
-            # Apply the stash
-            git stash pop --index
-        }
+
+        Resolve-Rebase $directory 
+        Invoke-SafePull $directory
     }
 }
 
